907b2244b1a2035439da6f225b65c38b1b4701c4
zstojkovic00 <00zeljkostojkovic@gmail.com>
2024-01-06 02:58:42 +0100

    Upgrade dependencies to the latest version, updated security configuration to adapt.

--- src/main/java/com/zeljko/gamelibrary/config/SecurityConfiguration.java
+++ src/main/java/com/zeljko/gamelibrary/config/SecurityConfiguration.java
@@ -7,6 +7,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
@@ -21,21 +22,17 @@ public class SecurityConfiguration {
 
         @Bean
         public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-                 http
-                    .csrf()
-                        .disable()
-                    .cors()
-                        .and()
-                    .authorizeHttpRequests()
-                        .requestMatchers("/api/v1/**").permitAll()
-                        .anyRequest().authenticated()
-                        .and()
-                    .sessionManagement()
-                        .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-                        .and()
-                    .authenticationProvider(authenticationProvider)
-                    .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class);
-            return http.build();
-        }
 
+            return http.csrf(AbstractHttpConfigurer::disable)
+                    .authorizeHttpRequests(auth -> auth
+                            .requestMatchers("/api/v1/**")
+                                    .permitAll()
+                                    .anyRequest()
+                                    .authenticated())
+                    .sessionManagement(sm ->
+                            sm.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                    .authenticationProvider(authenticationProvider)
+                    .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class)
+                    .build();
+        }
 }
